c6d820723496a6b73051770b507a6573846b151c
Helena <helena.mkopp@gmail.com>
2020-05-12 14:43:28 -0300

    Criação de testes e demais modulos - parte 2 (nao funcionando)

--- comandos.py
+++ comandos.py
@@ -1,6 +1,54 @@
 class Submarino(object):
 
+    def __init__(self):
+    #0 norte, 1 leste, 2 sul, 3 oeste
+        self.apontando_para = 0
+        self.z = 0
+        self.x = 0
+        self.y = 0
+
     def coordenada(self, comando=""):
-        return "2 3 -2 SUL"
+        for c in comando:
+            if c == "R":
+                self.right()
+            elif c == "L":
+                self.left()
+            elif c == "U":
+                self.up()
+            elif c == "D":
+                self.down()
+            elif c == "M":
+                self.movimentar()
+
+        direcao = ["NORTE", "LESTE", "SUL", "OESTE"][self.apontando_para]
+        return  "%s %s %s %s" % (self.x, self.y, self.z, direcao)
+
+    def movimentar(self):
+        if self.apontando_para == 0: #norte
+            self.y += 1
+        if self.apontando_para == 2: #sul
+            self.y -= 1
+        if self.apontando_para == 1: #leste
+            self.x += 1
+        if self.apontando_para == 3: #oeste
+            self.x -= 1
+
+        return True
+
+    def right(self):
+        self.apontando_para = 0 if self.apontando_para == 3 else self.apontando_para + 1
+        return self.apontando_para
+
+    def left(self):
+        self.apontando_para = 3 if self.apontando_para == 0 else self.apontando_para - 1
+        return self.apontando_para
+
+    def down(self):
+        self.z = self.z -1
+        return self.z
+
+    def up(self):
+        self.z = 0 if self.z == 0 else self.z + 1
+        return self.z
 
 
--- test_comandos.py
+++ test_comandos.py
@@ -8,30 +8,73 @@ class Test_Submarino(unittest.TestCase):
         sub = Submarino()
         self.assertEqual("2 3 -2 SUL", sub.coordenada("RMMLMMMDDLL"))    #passo os comandos como parametro e ele me retorna a coordenada.
 
-        #sub = Submarino()
-        #self.assertEqual("-1 2 0 NORTE", sub.coordenada("LMRDDMMUU"))    #exemplo de execução do próprio problema #precisa instanciar a classe p/ zerar a posição inicial do submarino
+        sub = Submarino()
+        self.assertEqual("-1 2 0 NORTE", sub.coordenada("LMRDDMMUU"))    #exemplo de execução do próprio problema #precisa instanciar a classe p/ zerar a posição inicial do submarino
 
-    #def testPosInicial(self):
-        #sub = Submarino()
-        #self.assertEqual("0 0 0 NORTE", sub.coordenada())      #posição inicial dada pelo problema
-
-    #       NORTE
-    #         0
-    #
-    # OESTE           LESTE
-    #   3               1
-    #
-    #         SUL
-    #          2
-    # O teste abaixo fala que, quando esta no apontando p/ esta no norte = 0
-    # Quando vira uma vez p esquerda, aponta para o 3 = oeste..
-    # Assim por diante.
+    def testPosInicial(self):
+        sub = Submarino()
+        self.assertEqual("0 0 0 NORTE", sub.coordenada())      #posição inicial dada pelo problema
 
 
-    def testPosicionamento(self):
+    def testPosicionamento(self):  #referente a bussula
         sub = Submarino()
         self.assertEqual(0, sub.apontando_para)
         self.assertEqual(3, sub.left)
         self.assertEqual(0, sub.right)
         self.assertEqual(1, sub.right)
         self.assertEqual(2, sub.right)
+
+    def testProfundidade(self): #cima e baixo
+        sub = Submarino()
+        self.assertEqual(0, sub.z)
+        self.assertEqual(0, sub.up())
+        self.assertEqual(-1, sub.down())
+        self.assertEqual(-2, sub.down())
+        self.assertEqual(-3, sub.down())
+        self.assertEqual(-2, sub.up())
+
+    def testMovimentarApontandoParaONorte(self):
+        sub = Submarino()
+        sub.apontando_para = 0
+
+        sub.movimentar()
+        self.assertEqual(0, sub.x)
+        self.assertEqual(1, sub.y)
+
+        sub.movimentar()
+        self.assertEqual(0, sub.x)
+        self.assertEqual(2, sub.y)
+
+        sub.movimentar()
+        self.assertEqual(0, sub.x)
+        self.assertEqual(3, sub.y)
+
+
+    def testMovimentarApontandoParaOSul(self):
+        sub = Submarino()
+        sub.apontando_para = 2
+
+        sub.movimentar()
+        self.assertEqual(0, sub.x)
+        self.assertEqual(-1, sub.y)
+
+        sub.movimentar()
+        self.assertEqual(0, sub.x)
+        self.assertEqual(-2, sub.y)
+
+        sub.movimentar()
+        self.assertEqual(0, sub.x)
+        self.assertEqual(-3, sub.y)
+
+
+    def testMovimantarApontandoParaOOeste(self):
+        sub = Submarino()
+        sub.apontando_para = 3
+
+        sub.movimentar()
+        self.assertEqual(-1, sub.x)
+        self.assertEqual(0, sub.y)
+
+        sub.movimentar()
+        self.assertEqual(-2, sub.x)
+        self.assertEqual(0, sub.y)
